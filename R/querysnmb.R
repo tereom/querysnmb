f223deb7895bb2e91d1d2f4220beb37a0ded45b4
Maria Teresa Ortiz <teresa.ortiz.mancera@mail.com>
2016-01-12 12:42:59 -0600

    Add documentation for package

---
+++ R/querysnmb.R
@@ -0,0 +1,14 @@
+#' querysnmb: A package for querying the SNMB database.
+#'
+#' The querysnmb package provides functions to query the SNMB database.
+#'
+#' @section Querysnmb functions:
+#' The functions use the dplyr package to query the database, the advantage of
+#' using them is that the query functions perform the necessary joins to have a
+#' useful table of information, and provide several variables to filter the
+#' results.
+#'
+#' @docType package
+#' @name querysnmb
+NULL
+#> NULL
